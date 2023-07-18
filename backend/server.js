68a9fa70172e95fd8bb5077172987cb3c8e79489
Pratham Shah <Prathamsanjayshah2001@gmail.com>
2023-07-18 22:43:13 +0530

    template

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { Server } from "socket.io";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/geoHealthDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongoDB");
+});
+
+// import "./modals/User.js";
+// import { typeDefs } from "./schema.js";
+// import { resolvers } from "./resolvers.js";
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+// const { url } = await startStandaloneServer(server, {
+//   context: async ({ req }) => {
+//     const { authorization } = req.headers;
+//     if (authorization) {
+//       const { userId } = jwt.verify(authorization, "avbdd!@#$]");
+//       return { userId: userId };
+//     }
+//   },
+// });
+// console.log(`🚀 Server ready at ${url}`);
---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+// added dependencies: 
+// +-- @apollo/client@3.7.17
+// +-- @reduxjs/toolkit@1.9.5
+// +-- @testing-library/jest-dom@5.17.0
+// +-- @testing-library/react@13.4.0
+// +-- @testing-library/user-event@13.5.0
+// +-- @types/jest@27.5.2
+// +-- @types/react@18.2.15
+// +-- @types/react-dom@18.2.7
+// +-- react@18.2.0
+// +-- react-dom@18.2.0
+// +-- react-redux@8.1.1
+// +-- react-router@6.14.2
+// +-- react-scripts@5.0.1
+// +-- socket.io-client@4.7.1
+// +-- typescript@4.9.5
+// `-- web-vitals@2.1.4
+const App = () => {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
